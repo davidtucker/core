9bc0aea825d3ca233775c806c504774b39a5e6a0
Serkan Sipahi <serkan.sipahi@yahoo.de>
2020-03-25 16:06:17 +0100

    cleanup

--- jasmine.node.config.ts
+++ jasmine.node.config.ts
@@ -3,26 +3,26 @@ const Jasmine = require('jasmine');
 // @ts-ignore
 const jasmine = new Jasmine({});
 jasmine.loadConfig({
-    spec_dir: '**/*/test',
-    spec_files: ['**/*.node.spec.ts'],
-    random: false,
-    seed: null,
-    stopSpecOnExpectationFailure: false
+  spec_dir: '**/*/test',
+  spec_files: ['**/*.node.spec.ts'],
+  random: false,
+  seed: null,
+  stopSpecOnExpectationFailure: false
 });
 jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
 
 // setup console reporter
 const JasmineConsoleReporter = require('jasmine-console-reporter');
 const reporter = new JasmineConsoleReporter({
-    colors: 1,
-    cleanStack: 1,
-    verbosity: 4,
-    listStyle: 'indent',
-    timeUnit: 'ms',
-    timeThreshold: { ok: 500, warn: 1000, ouch: 3000 },
-    activity: true,
-    emoji: true,
-    beep: true
+  colors: 1,
+  cleanStack: 1,
+  verbosity: 4,
+  listStyle: 'indent',
+  timeUnit: 'ms',
+  timeThreshold: { ok: 500, warn: 1000, ouch: 3000 },
+  activity: true,
+  emoji: true,
+  beep: true
 });
 
 jasmine.env.clearReporters();
--- karma.conf.ts
+++ karma.conf.ts
@@ -1,71 +1,72 @@
 module.exports = config => {
-    config.set({
-        basePath: '',
-        frameworks: [
-            'esm',
-            'jasmine',
+  config.set({
+    basePath: '',
+    frameworks: [
+      'esm',
+      'jasmine',
+    ],
+    files: [
+      file('packages/**/*.ts', config),
+      file('test/**/*.ts', config),
+      file('src/**/*.ts', config),
+    ],
+    exclude: excludeFromTesting,
+    browsers: [
+      'Chrome',
+      'Firefox',
+      'Opera',
+      'Safari',
+    ],
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-opera-launcher',
+      'karma-safari-launcher',
+      require.resolve('@open-wc/karma-esm'),
+    ],
+    esm: {
+      babel: true,
+      customBabelConfig: {
+        presets: [
+            "@babel/preset-typescript",
         ],
-        files: [
-            file('packages/**/*.ts', config),
-            file('test/**/*.ts', config),
-            file('src/**/*.ts', config),
-        ],
-        exclude: excludeFromTesting,
-        browsers: [
-            'Chrome',
-            'Firefox',
-            'Opera',
-            'Safari',
-        ],
-        plugins: [
-            'karma-jasmine',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-opera-launcher',
-            'karma-safari-launcher',
-            require.resolve('@open-wc/karma-esm'),
-        ],
-        esm: {
-            babel: true,
-            customBabelConfig: {
-                presets: [
-                    "@babel/preset-typescript",
-                ],
-            },
-            babelExclude: excludeFromCompilation,
-            babelModernExclude: excludeFromCompilation,
-            babelModuleExclude: excludeFromCompilation,
+      },
+      babelExclude: excludeFromCompilation,
+      babelModernExclude: excludeFromCompilation,
+      babelModuleExclude: excludeFromCompilation,
 
-            nodeResolve: true,
-            fileExtensions: ['.ts'],
-            preserveSymlinks: true,
+      nodeResolve: true,
+      fileExtensions: ['.ts'],
+      preserveSymlinks: true,
 
-            polyfills: {
-                custom: customPolyfills,
-            },
-        },
-    });
-    return config;
+      polyfills: {
+        custom: customPolyfills,
+      },
+    },
+  });
+  return config;
 };
 
-const file = (filePattern, config) => ({
-    pattern: config.grep ? config.grep : filePattern,
-    type: 'module',
+const file = (filePattern: string, config: {grep: string}) => ({
+  pattern: config.grep ? config.grep : filePattern,
+  type: 'module',
 });
 
 const excludeFromCompilation = [
-    '**/node_modules/**/*.js',
-    '**/node_modules/**/*.ts',
+  '**/node_modules/**/*.js',
+  '**/node_modules/**/*.ts',
 ];
 
+// @todo: this can be remove when adjust test files to: foo.browser.spec.ts or foo.node.spec.ts
 const excludeFromTesting = [
-    'packages/pg-compiler-template/**/*.ts',
+  'packages/pg-compiler-template/**/*.ts',
 ];
 
 const customPolyfills = [
-    {
-        name: 'document-register-element',
-        path: require.resolve('document-register-element'),
-        test: "!('customElements' in window)",
-    },
+  {
+    name: 'document-register-element',
+    path: require.resolve('document-register-element'),
+    test: "!('customElements' in window)",
+  },
 ];
