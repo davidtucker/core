25941dc1adb70ba8910609f7428150afef9ce0bd
Serkan Sipahi <serkan.sipahi@yahoo.de>
2020-03-30 18:53:16 +0200

    work in progress

--- babel.compiler.js
+++ babel.compiler.js
@@ -1,5 +1,6 @@
 const watch = require('node-watch');
 const shell = require('shelljs');
+const util = require('util');
 
 // options
 const watchOptions = {
@@ -7,46 +8,45 @@ const watchOptions = {
   filter: /^((?!(node_modules|\.cjs|\.js|\.d\.ts)).)*$/,
 };
 const shellOptions = {
-  async: true
+  async: true,
 };
 
-// bins
-const babel = './node_modules/.bin/babel';
-const tsc = './node_modules/.bin/tsc';
-const babelPlugins = '--plugins @babel/plugin-transform-modules-commonjs';
-
-// npm scripts
+// npm scripts: clean
 const exeClean = 'npm run clean';
-const execBabelCjsModuleAll = 'npm run compile:cjs:all';
+
+// npm scripts: compile all files to .d.ts, .js, .cjs
 const execBabelJsModuleAll = 'npm run compile:mjs:all';
+const execBabelCjsModuleAll = 'npm run compile:cjs:all';
 const execTsDeclarationsAll = 'npm run compile:dec:all';
+
+// npm scripts: compile single file to .d.ts, .js, .cjs
+const execBabelJsModuleSingle = 'npm run compile:mjs:single --src-file=%s --out-file=%s';
+const execBabelCjsModuleSingle = 'npm run compile:cjs:single --src-file=%s --out-file=%s';
+const execTsDeclarationsSingle = 'npm run compile:dec:single --src-file=%s';
+
+// npm scripts: combine tasks
 const execBabel = [exeClean, execBabelCjsModuleAll, execBabelJsModuleAll, execTsDeclarationsAll].join('&& '); // '&& ' => run in shell sequentially
 
 // helper
+const replaceExtToJs = name => name.replace('.ts', '.js');
+const replaceExtToCJs = name => name.replace('.ts', '.cjs');
+const replaceExtToDTs = name => name.replace('.ts', '.d.ts');
 const compiledMessage = name => console.log('Successfully compiled 1 file with Babel: %s', name);
-const replaceToJs = name => name.replace('.ts', '.js');
-const replaceToCJs = name => name.replace('.ts', '.cjs');
-const replaceToDTs = name => name.replace('.ts', '.d.ts');
+const hasError = (code, stderr) => code !== 0 && stderr;
 
 // First compile all files to .js and .cjs
 shell.exec(execBabel, (code, stdout, stderr) => {
-
   // Then compile particular file on change
   watch('./packages', watchOptions, (_, name) => {
-
-    // to esModule
-    shell.exec(`${babel} ${name} --out-file ${replaceToJs(name)} --source-maps inline`, shellOptions,
-      _ => code !== 0 && stderr ? null : compiledMessage(replaceToJs(name)),
-    );
-
-    // to commonjs
-    shell.exec(`${babel} ${name} --out-file ${replaceToCJs(name)} ${babelPlugins} --source-maps inline`, shellOptions,
-      _ => code !== 0 && stderr ? null : compiledMessage(replaceToCJs(name)),
-    );
-
-    // to definition file
-    shell.exec(`${tsc} ${name} --emitDeclarationOnly --declaration`, shellOptions,
-      _ => code !== 0 && stderr ? null : compiledMessage(replaceToDTs(name)),
-    );
+    shell.exec(util.format(execBabelJsModuleSingle, name, replaceExtToJs(name)), shellOptions,
+      _ => hasError(code, stderr) ? null : compiledMessage(replaceExtToJs(name)));
+    shell.exec(util.format(execBabelCjsModuleSingle, name, replaceExtToCJs(name)), shellOptions,
+      _ => hasError(code, stderr) ? null : compiledMessage(replaceExtToCJs(name)));
+    shell.exec(util.format(execTsDeclarationsSingle, name), shellOptions,
+      _ => hasError(code, stderr) ? null : compiledMessage(replaceExtToDTs(name)));
   });
 });
+
+// bonus: write babel.compiler.js in golang
+// https://golang.org/pkg/os/exec/
+// https://github.com/fsnotify/fsnotify
