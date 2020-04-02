1e707c04251d190c4f1674b253b15bc88e5e2c71
Serkan Sipahi <serkan.sipahi@yahoo.de>
2020-04-02 15:52:50 +0200

    test

--- packages/pkg-browser-dom/src/pg-browser-dom-utils.ts
+++ packages/pkg-browser-dom/src/pg-browser-dom-utils.ts
@@ -1,8 +1,8 @@
 export const updateDom = (context: HTMLElement, name: string, value: any) => {
   const [uuid, expressionValue] = context[name];
-  // if (expressionValue === value) {
-  //   return;
-  // }
+  if (expressionValue === value) {
+    return;
+  }
 
   const element = context.querySelector(uuid);
   if (element) {
