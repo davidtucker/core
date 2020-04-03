0b06667e7108486d31ee8db8fd29624fe66dad7d
Serkan Sipahi <serkan.sipahi@yahoo.de>
2020-04-03 13:17:37 +0200

    work in progress

--- packages/pkg-node-html-compiler/test/pg-node-html-compiler.spec.ts
+++ packages/pkg-node-html-compiler/test/pg-node-html-compiler.spec.ts
@@ -1,6 +1,6 @@
 describe('Pregular node compile template', () => {
   it('should test', () => {
     expect(123).toEqual(123);
-    console.log('Pregular node compile template');
+    console.log('Pregular node compile template!');
   });
 });
